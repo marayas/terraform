645b0dc544d5b3711dfacf24b3707ec434c7d7f5
marayas <69701996+marayas@users.noreply.github.com>
2023-06-29 10:23:36 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,4 +12,10 @@ provider "aws" {
   region = var.aws_region
   #access_key = var.AWS_ACCESS_KEY_ID
   #secret_key = var.AWS_SECRET_ACCESS_KEY
+
+  assume_role {
+     role_arn = "arn:aws:iam::${var.account_id}:role/${data.vault_kv_secret_v2.terraformsecret.data.aws_account_access}"
+   }
+  sts_region = "us-west-2"
+
   }
