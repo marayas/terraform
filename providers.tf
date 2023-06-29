daa4cfd150421364d2e5696373b575f822cbf66a
marayas <69701996+marayas@users.noreply.github.com>
2023-06-29 09:47:17 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,5 +11,5 @@ provider "aws" {
   # Configuration options
   region = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = varAWS_SECRET_ACCESS_KEY
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   }
